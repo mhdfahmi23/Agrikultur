975db56f44866e34696557e2535df503b92eb5ec
mhdfahmi <44706387+mhdfahmi@users.noreply.github.com>
2018-11-21 21:34:27 +0700

    inovator

--- application/views/inovator.php
+++ application/views/inovator.php
@@ -5,7 +5,25 @@
 
           <div class="card">
             <h2>Tokoh Pertanian</h2>
-            <p>&#9829;</p>
+            <h3>1. Bachtiar Karim</h3>
+            <div class="fakeimg"><img src="assets/images/b_karim.jpg"></div>
+            <p>Masuk ke dalam peringkat ke lima orang terkaya di Indonesia. Menurut Forbes kekayaanya mencapai $3,3  Billon. Pria yang tak terlalu terkenal di kalangan pengusaha ini bersama kedua saudaranya  Burhan dan Bahari menjalankan perusahaan yang bernama PT Musim Mas. Perusahaan tersebut masuk ke dalam industri minyak sawit terbesar di Indonesia bahkan masuk ke dalam salah satu perusahaan oleo terbesar di dunia.
+
+Perusahaan yang didirikan oleh ayahnya, Anwar Karim pada tahun 1972 tersebut awalnya hanyalah perusahaan sabun dan margarin yang tadinya bernama Nam Cheong Soap Factory. Namun kemudian diubah oleh Bachtiar Karim menjadi Musim Mas dan bukan hanya bergerak dalam pengolahan sabun namun juga mengembangkannya bisnisnya hingga menjadi perusahaan fasilitasi kilang minyak sawit yang besar. Dan di tahun 1988 Bachtiar berhasil memiliki perkebunan kelapa sawit sendiri.
+
+
+ 
+Tak hanya itu pria lulusan Teknik tersebut juga berkontribusi dalam membangun teknis perusahaan  dan meningkatkan produksi perusahaan yang tadinya hanya 120 ton perbulan menjadi 400 ton perbulannya. Kini perusahaan tersebut di tahun 2015 telah menandatangi perjanjian kerjasama dengan perusahaan kelapa sawit Malaysia Genting Plantation untuk membangun kilang minyak sawit di Pulau Kalimantan.
+
+Perusahaan yang dijalankan oleh Bachtiar menggunakan berbagai inovasi salah satunya dengan menggunakan energi yang lebih rendah di saat orang  ingin mengurangi resiko kebocoran. Dan kini 90% produksi perusahaanya untuk ekspor sedangkan tiga produk yang dikenal di Indonesia seperti Medicare, Lark dan sabun batangan Harmony. Selain itu Bachtiar karim juga juga menyuplai minyak sawit mentah di perusahaannya.
+
+Eddy Kusnadi Sariaatmadja
+
+Pria kelahiran 11 Desember 1963 ini berada di peringkat ke 10 orang terkaya di Indonesia. Selain menjadi pemegang saham terbesar di dua stasiun televisi Indonesia, SCTV dan Indosiar. Pria kelahiran Jakarta ini juga berbisnis di sektor pertanian.
+
+Anak dari pasangan  Mohamad Soeboeb Sariatmadja dan Sofi Wijaya ini memiliki perusahaan perkebunan tertua di Indonesia dengan nama Pt London Sumatera Plantation. Meskipun diawal perkebunan ini milik Inggris namun Eddy berhasil memegang 64,4% sahamnya sekarang.  Perusahaan penghasil karet, kelapa sawit, kopi, kakao dan lainnya tersebut ternyata berhasil mendongkrak kekayaan pria lulusan University of New South Wales Australia.
+
+Selain memiliki bisnis di bidang pertelevisian dan pertanian, pria yang memiliki kekayaan hingga $1,38 Billon ini  juga berhasil dalam bisnis komputer. Dengan kemampuannya yang merupakan lulusan teknik dia berhasil menjalankan bisnis yang menjadi distributor komputer Compaq di Indonesia dengan nama perusahaan PT Elang Mahkota (EMTEK) yang kini tumbuh pesat di Indonesia dan berhasil membeli 20 persen saham Bobobobo.com sebuah perusahaan yang menjual produk gaya hidup.</p>
 
 
 
@@ -16,8 +34,13 @@
 
         <div class="card">
 
-            <h2>Tokoh Pertanian 2</h2>
-            <p>Mengembangkan Pertanian dengan Hidroponik</p>
+            <h3>2. Eddy Kusnadi Sariaatmadja</h3>
+            <div class="fakeimg"><img src="assets/images/eddy.jpg"></div>
+            <p>Pria kelahiran 11 Desember 1963 ini berada di peringkat ke 10 orang terkaya di Indonesia. Selain menjadi pemegang saham terbesar di dua stasiun televisi Indonesia, SCTV dan Indosiar. Pria kelahiran Jakarta ini juga berbisnis di sektor pertanian.
+
+Anak dari pasangan  Mohamad Soeboeb Sariatmadja dan Sofi Wijaya ini memiliki perusahaan perkebunan tertua di Indonesia dengan nama Pt London Sumatera Plantation. Meskipun diawal perkebunan ini milik Inggris namun Eddy berhasil memegang 64,4% sahamnya sekarang.  Perusahaan penghasil karet, kelapa sawit, kopi, kakao dan lainnya tersebut ternyata berhasil mendongkrak kekayaan pria lulusan University of New South Wales Australia.
+
+Selain memiliki bisnis di bidang pertelevisian dan pertanian, pria yang memiliki kekayaan hingga $1,38 Billon ini  juga berhasil dalam bisnis komputer. Dengan kemampuannya yang merupakan lulusan teknik dia berhasil menjalankan bisnis yang menjadi distributor komputer Compaq di Indonesia dengan nama perusahaan PT Elang Mahkota (EMTEK) yang kini tumbuh pesat di Indonesia dan berhasil membeli 20 persen saham Bobobobo.com sebuah perusahaan yang menjual produk gaya hidup.</p>
             <div class="fakeimg" style="height:300px ; width:750px;"><img src="assets/ino1.png"></div>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
