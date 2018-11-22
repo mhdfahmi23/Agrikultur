2a802b2263d63ef0951f149a6cd06a5332e954cd
Rizki Juliansyah <rizki.juliansyah.a.m@gmail.com>
2018-11-23 06:46:54 +0700

    Update wawasan.php

--- application/views/wawasan.php
+++ application/views/wawasan.php
@@ -1,8 +1,35 @@
 <?php
     include 'header.php';
+
+    $koneksi = new mysqli("localhost", "root", "", "agri");
 ?>
 
-<p>wawasan</p>
+<h2>Wawasan</h2>
+
+<table class="table table-bordered">
+
+
+  <tbody>
+    <?php $nomor=1; ?>
+    <?php $ambil=$koneksi->query("select *from wawasan"); ?>
+    <?php while($pecah = $ambil->fetch_assoc()){ ?>
+    <h3><?php echo $pecah['judul_w']; ?></h3>
+    <?php echo $pecah['foto_w']; ?>
+    <?php echo $pecah['date_w']; ?>
+    <?php echo $pecah['isi_w']; ?>
+
+      </td>
+      </tr>
+      <?php $nomor++; ?>
+    <?php } ?>
+  </tbody>
+
+</table>
+
+
+
+
+
 
 <?php
     include 'footer.php';
