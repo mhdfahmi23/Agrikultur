277cd6d190c789e381884d015388cb7e8a61453a
Gilang Ramadhan <45072805+ggram1998@users.noreply.github.com>
2018-11-29 23:31:39 +0700

    Add files via upload
    Tambahan file Tentang berisi anggota grup

---
+++ application/views/tentang.php
@@ -0,0 +1,122 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us</title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
+  <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/AboutUs.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<div class="container">
+	<div class = "row">
+		<div class = "penjelasan" style = "text-align: center;">
+		<center><p>PENJELASAN SINGKAT</p></center>
+		<br>
+<p>AGRIKULTUR adalah aplikasi berbasis web yang menyediakan informasi tentang Dunia Agrikultur di Indonesia. adapun beberapa fitur yang ditampilkan pada website ini yakni terdapat Inovator, Acara, Ebook, dan Konsultasi. Fitur yang ditampilkan akan membantu pembaca dalam melakukan pengembangan terhadap dunia agrikultur </p>
+<br>
+<p> TENTANG KAMI!!!! </p>
+<br>
+
+</div>
+		<div class="col-sm-4">
+			<div class="card">
+			      <center><img src="<?=base_url()?>assets/img/AboutUs/Apin.jpeg" alt="cawi" style="width:330px"></center>
+			      <div class="container">
+			      	<br>
+			        <center><h4>Reza Pahlevi</h4></center>
+			        <center><p class="title">Ketua Kelompok</p></center>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/person-icon-5.PNG" alt="gitung" style="width:25px">
+			        Reza Pahlevi</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/credit-card_icon-icons.com_64588.PNG" alt="gitung" style="width:25px">
+			        14116002</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/appdev-icon-300x300.PNG" alt="gitung" style="width:25px">
+			        Teknik Informatika</p>
+			    </div>
+		    </div>
+		</div>
+</div>
+		<div class="col-sm-4">
+			<div class="card">
+			      <center><img src="<?=base_url()?>assets/img/AboutUs/Apin.jpeg" alt="cawi" style="width:330px"></center>
+			      <div class="container">
+			      	<br>
+			        <center><h4>Gilang Ramadhan</h4></center>
+			        <center><p class="title">Design and Front End</p></center>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/person-icon-5.PNG" alt="gitung" style="width:25px">
+			        Gilang Ramadhan</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/credit-card_icon-icons.com_64588.PNG" alt="gitung" style="width:25px">
+			        14116055</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/appdev-icon-300x300.PNG" alt="gitung" style="width:25px">
+			        Teknik Informatika</p>
+			    </div>
+		    </div>
+		</div>
+</div>
+		<div class="col-sm-4">
+			<div class="card">
+			      <center><img src="<?=base_url()?>assets/img/AboutUs/Apin.jpeg" alt="cawi" style="width:330px"></center>
+			      <div class="container">
+			      	<br>
+			        <center><h4>Fardiansyah S. Harahap</h4></center>
+			        <center><p class="title">Back End</p></center>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/person-icon-5.PNG" alt="gitung" style="width:25px">
+			        Fardiansyah S. Harahap</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/credit-card_icon-icons.com_64588.PNG" alt="gitung" style="width:25px">
+			        14115063</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/appdev-icon-300x300.PNG" alt="gitung" style="width:25px">
+			        Teknik Informatika</p>
+			    </div>
+		    </div>
+		</div>
+</div>
+		<div class="col-sm-4">
+			<div class="card">
+			      <center><img src="<?=base_url()?>assets/img/AboutUs/Apin.jpeg" alt="cawi" style="width:330px"></center>
+			      <div class="container">
+			      	<br>
+			        <center><h4>Muhammad Fahmi Akbar</h4></center>
+			        <center><p class="title">Database and Design</p></center>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/person-icon-5.PNG" alt="gitung" style="width:25px">
+			        Muhammad Fahmi Akbar</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/credit-card_icon-icons.com_64588.PNG" alt="gitung" style="width:25px">
+			        14116152</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/appdev-icon-300x300.PNG" alt="gitung" style="width:25px">
+			        Teknik Informatika</p>
+			    </div>
+		    </div>
+		</div>
+</div>
+		<div class="col-sm-4">
+			<div class="card">
+			      <center><img src="<?=base_url()?>assets/img/AboutUs/Apin.jpeg" alt="cawi" style="width:330px"></center>
+			      <div class="container">
+			      	<br>
+			        <center><h4>Rizky Juliansyah</h4></center>
+			        <center><p class="title">Back End</p></center>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/person-icon-5.PNG" alt="gitung" style="width:25px">
+			        Rizky Juliansyah</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/credit-card_icon-icons.com_64588.PNG" alt="gitung" style="width:25px">
+			        14116153</p>
+			        <p><img src="<?=base_url()?>assets/img/AboutUs/appdev-icon-300x300.PNG" alt="gitung" style="width:25px">
+			        Teknik Informatika</p>
+			    </div>
+		    </div>
+		</div>
+
+
+	
+	</div>
+</div> 
+</body>
+</html>
